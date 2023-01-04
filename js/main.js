bb55cd28c5db3180e38e1879f262999c93dbd3e5
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2023-01-04 14:22:44 +0100

    js_comparisons

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let num = 5;
+let anotherNum = 8;
+
+console.log(num == anotherNum);
+console.log(num > anotherNum);
+console.log(num < anotherNum);
+console.log(num >= anotherNum);
+console.log(num <= anotherNum);
+
+console.log(num == "5");
+console.log(num != 8);
+console.log(num != "5");
